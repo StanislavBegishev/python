83a8e4464acc0568cfbd0040eca3b61b6169b1fa
Stas <stanislav.begishev@mail.ru>
2022-11-23 08:05:28 +0700

    additional_task_5

--- hw_1/additional_task_5.py
+++ hw_1/additional_task_5.py
@@ -5,11 +5,11 @@
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
 
-xA = int(input('Введите xA'))
-yA = int(input('Введите yA'))
+xA = int(input('Введите xA: '))
+yA = int(input('Введите yA: '))
 
-xB = int(input('Введите xB'))
-yB = int(input('Введите yB'))
+xB = int(input('Введите xB: '))
+yB = int(input('Введите yB: '))
 
 AB = ((xB - xA) ** 2 + (yB - yA) ** 2)**(0.5)
 
